4296bd044ec1b02ad02a8a89633574ea00522e99
SeaSalvageUnit <comcool93@gmail.com>
2019-12-02 20:53:29 +0900

    Header 작성, 페이지마다 다른 Header 할 수 있게 할 것 2019/12/02 PM 8:53

--- process.go
+++ process.go
@@ -3,7 +3,26 @@ package main
 import (
 	"github.com/labstack/echo"
 	"net/http"
+	"time"
 )
+
+// TODO -- Header 작성
+
+// InstallProcessHeader 미들웨어 함수는 Header를 추가합니다.
+func InstallProcessHeader(next echo.HandlerFunc) echo.HandlerFunc {
+	// TODO --- Header가 일정 페이지에서만 작동하게 하는 거 찾을 것.
+	return func(c echo.Context) error {
+		c.Response().Header().Set("Expires", "0")
+		c.Response().Header().Set(echo.HeaderLastModified, time.Now().Format("Mon, 02 Dec 2019 09:56:15 GMT")) // Mon, 02 Dec 2019 09:56:15 GMT
+		c.Response().Header().Set("Cache-Control", "no-store, no-cache, must-revalidate")
+		c.Response().Header().Set("Cache-Control", "pre-check=0, post-check=0, max-age=0")
+		c.Response().Header().Set("Pragma", "no-cache")
+		c.Response().Header().Set(echo.HeaderContentType, "text/html; charset=utf-8")
+		c.Response().Header().Set("X-Robots-Tag", "noindex")
+
+		return next(c)
+	}
+}
 
 // TODO - 인스톨 프로세스 작성할 것.
 func InstallProcess(c echo.Context) error {
